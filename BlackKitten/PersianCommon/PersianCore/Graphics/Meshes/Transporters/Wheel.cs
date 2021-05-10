d1a38c2284e3b7f98de056460b415e5734c99fe5
PooyaEimandar <pooya.eimandar@live.com>
2021-05-10 12:49:46 +0430

    PersianEngine (codename: BlackKitten)

---
+++ BlackKitten/PersianCommon/PersianCore/Graphics/Meshes/Transporters/Wheel.cs
@@ -0,0 +1,373 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using PhysicEngine;
+//using PhysicEngine.Dynamics;
+//using PhysicEngine.Collision;
+//using PhysicEngine.LinearMath;
+
+//namespace PersianCore.Physic
+//{
+//    /// <summary>
+//    /// Enumeration for the four car wheels.
+//    /// </summary>
+//    public enum WheelPosition
+//    {
+//        /// <summary>Front left wheel.</summary>
+//        FrontLeft,
+//        /// <summary>Front right wheel.</summary>
+//        FrontRight,
+//        /// <summary>Back left wheel.</summary>
+//        BackLeft,
+//        /// <summary>Back right wheel.</summary>
+//        BackRight
+//    }
+
+//    /// <summary>
+//    /// A wheel which adds drive forces to a body.
+//    /// Can be used to create a vehicle.
+//    /// </summary>
+//    class Wheel
+//    {
+//        private RigidBody BodyCar;
+
+//        private float displacement, upSpeed, lastDisplacement;
+//        private bool lastOnFloor;
+//        private float driveTorque;
+
+//        private float angVel;
+
+//        /// used to estimate the friction
+//        private float angVelForGrip;
+//        private float torque;
+
+//        private RaycastCallback raycast;
+
+//        /// <summary>
+//        /// Sets or gets the current steering angle of
+//        /// the wheel in degrees.
+//        /// </summary>
+//        public float SteerAngle { get; set; }
+
+//        /// <summary>
+//        /// Gets the current rotation of the wheel in degrees.
+//        /// </summary>
+//        public float WheelRotation { get; private set; }
+
+//        /// <summary>
+//        /// The damping factor of the supension spring.
+//        /// </summary>
+//        public float Damping { get; set; }
+
+//        /// <summary>
+//        /// The supension spring.
+//        /// </summary>
+//        public float Spring { get; set; }
+
+//        /// <summary>
+//        /// Inertia of the wheel.
+//        /// </summary>
+//        public float Inertia { get; set; }
+
+//        /// <summary>
+//        /// The wheel radius.
+//        /// </summary>
+//        public float Radius { get; set; }
+
+//        /// <summary>
+//        /// The friction of the car in the side direction.
+//        /// </summary>
+//        public float SideFriction { get; set; }
+
+//        /// <summary>
+//        /// Friction of the car in forward direction.
+//        /// </summary>
+//        public float ForwardFriction { get; set; }
+
+//        /// <summary>
+//        /// The length of the suspension spring.
+//        /// </summary>
+//        public float WheelTravel { get; set; }
+
+//        /// <summary>
+//        /// If set to true the wheel blocks.
+//        /// </summary>
+//        public bool Locked { get; set; }
+
+//        /// <summary>
+//        /// The highest possible velocity of the wheel.
+//        /// </summary>
+//        public float MaximumAngularVelocity { get; set; }
+
+//        /// <summary>
+//        /// The number of rays used for this wheel.
+//        /// </summary>
+//        public int NumberOfRays { get; set; }
+
+//        /// <summary>
+//        /// The position of the wheel in body space.
+//        /// </summary>
+//        public JVector Position { get; set; }
+
+//        /// <summary>
+//        /// Creates a new instance of the Wheel class.
+//        /// </summary>
+//        /// <param name="world">The world.</param>
+//        /// <param name="car">The RigidBody on which to apply the wheel forces.</param>
+//        /// <param name="position">The position of the wheel on the body (in body space).</param>
+//        /// <param name="radius">The wheel radius.</param>
+//        public Wheel(RigidBody BodyCar, JVector position, float radius)
+//        {
+//            this.BodyCar = BodyCar;
+//            this.Position = position;
+            
+//            raycast = new RaycastCallback(RaycastCallback);
+
+//            // set some default values.
+//            this.SideFriction = 1.5f;
+//            this.ForwardFriction = 1f;
+//            this.Radius = radius;
+//            this.Inertia = 1.0f;
+//            this.WheelTravel = 0.2f;
+//            this.MaximumAngularVelocity = 200;
+//            this.NumberOfRays = 5;
+//        }
+
+//        /// <summary>
+//        /// Gets the position of the wheel in world space.
+//        /// </summary>
+//        /// <returns>The position of the wheel in world space.</returns>
+//        public JVector GetWorldPosition()
+//        {
+//            return BodyCar.Position +
+//                JVector.Transform(Position + JVector.Up * displacement, BodyCar.Orientation);
+//        }
+
+//        /// <summary>
+//        /// Adds drivetorque.
+//        /// </summary>
+//        /// <param name="torque">The amount of torque applied to this wheel.</param>
+//        public void AddTorque(float torque)
+//        {
+//            driveTorque += torque;
+//        }
+
+//        public void PostStep(float timeStep)
+//        {
+//            if (timeStep <= 0.0f) return;
+
+//            float origAngVel = angVel;
+//            upSpeed = (displacement - lastDisplacement) / System.Math.Max(timeStep, JMath.Epsilon);
+
+//            if (Locked)
+//            {
+//                angVel = 0;
+//                torque = 0;
+//            }
+//            else
+//            {
+//                angVel += torque * timeStep / Inertia;
+//                torque = 0;
+
+//                if (!lastOnFloor) driveTorque *= 0.1f;
+
+//                // prevent friction from reversing dir - todo do this better
+//                // by limiting the torque
+//                if (((origAngVel > angVelForGrip) && (angVel < angVelForGrip)) ||
+//                     ((origAngVel < angVelForGrip) && (angVel > angVelForGrip)))
+//                    angVel = angVelForGrip;
+
+//                angVel += driveTorque * timeStep / Inertia;
+//                driveTorque = 0;
+
+//                float maxAngVel = this.MaximumAngularVelocity;
+//                angVel = JMath.Clamp(angVel, -maxAngVel, maxAngVel);
+
+//                WheelRotation += (timeStep * angVel) / (2*JMath.Pi) * 360.0f;
+//            }
+//        }
+
+//        public void PreStep(float timeStep)
+//        {
+//            float vel = BodyCar.LinearVelocity.Length();
+
+//            SideFriction = 2.5f - JMath.Clamp(vel / 20.0f, 0.0f, 1.4f);
+//            ForwardFriction = 5.5f - JMath.Clamp(vel / 20.0f, 0.0f, 5.4f);
+
+//            JVector force = JVector.Zero;
+
+//            JVector worldAxis = JVector.Transform(JVector.Up, BodyCar.Orientation);
+//            JVector worldPos = BodyCar.Position + JVector.Transform(Position, BodyCar.Orientation);
+
+//            JVector forward = new JVector(-BodyCar.Orientation.M31, -BodyCar.Orientation.M32, -BodyCar.Orientation.M33);
+
+//            JVector wheelFwd = JVector.Transform(forward, JMatrix.CreateFromAxisAngle(JVector.Up, SteerAngle / 360 * 2 * JMath.Pi));
+//            JVector wheelLeft = JVector.Cross(JVector.Up, wheelFwd); wheelLeft.Normalize();
+//            JVector wheelUp = JVector.Cross(wheelFwd, wheelLeft);
+
+//            float rayLen = 2.0f * Radius + WheelTravel;
+            
+//            JVector wheelRayStart = worldPos;
+//            JVector wheelDelta = -Radius * worldAxis;
+//            JVector wheelRayEnd = worldPos + wheelDelta;
+
+//            float deltaFwd = (2.0f * Radius) / (NumberOfRays + 1);
+//            float deltaFwdStart = deltaFwd;
+
+//            lastDisplacement = displacement;
+//            displacement = 0.0f;
+
+//            lastOnFloor = false;
+
+//            JVector rayOrigin = BodyCar.Position + JVector.Transform(Position, BodyCar.Orientation);
+
+//            JVector groundNormal = JVector.Zero;
+//            JVector groundPos = JVector.Zero;
+//            float deepestFrac = float.MaxValue;
+//            RigidBody worldBody = null;
+
+//            for (int i = 0; i < NumberOfRays; i++)
+//            {
+//                float distFwd = (deltaFwdStart + i * deltaFwd) - Radius;
+//                float zOffset = Radius * (1.0f - (float)Math.Cos(Math.PI / 4 * (distFwd / Radius)));
+
+//                JVector newOrigin = wheelRayStart + distFwd * wheelFwd + zOffset * wheelUp;
+
+//                RigidBody body; JVector normal; float frac;
+//                bool result = PhysicManager.physicWorld.CollisionSystem.Raycast(newOrigin, wheelDelta,
+//                    raycast, out body, out normal, out frac);
+
+ 
+
+//                if (result && frac <= 1.0f)
+//                {
+//                    if (frac < deepestFrac)
+//                    {
+//                        deepestFrac = frac;
+//                        groundPos = rayOrigin + frac * wheelDelta;
+//                        worldBody = body;
+//                        groundNormal = normal;
+//                    }
+
+//                    lastOnFloor = true;
+//                }
+//            }
+
+//            if (!lastOnFloor) return;
+
+//            if (groundNormal.LengthSquared() > 0.0f) groundNormal.Normalize();
+
+//           // System.Diagnostics.Debug.WriteLine(groundPos.ToString());
+            
+
+//            displacement = rayLen * (1.0f - deepestFrac);
+//            displacement = JMath.Clamp(displacement, 0.0f,WheelTravel);
+
+//            float displacementForceMag = displacement * Spring;
+
+//            // reduce force when suspension is par to ground
+//            displacementForceMag *= Math.Abs(JVector.Dot(groundNormal, worldAxis));
+
+//            // apply damping
+//            float dampingForceMag = upSpeed * Damping;
+
+//            float totalForceMag = displacementForceMag + dampingForceMag;
+
+//            if (totalForceMag < 0.0f) totalForceMag = 0.0f;
+
+//            JVector extraForce = totalForceMag * worldAxis;
+
+//            force += extraForce;
+
+//            JVector groundUp = groundNormal;
+//            JVector groundLeft = JVector.Cross(groundNormal, wheelFwd);
+//            if (groundLeft.LengthSquared() > 0.0f) groundLeft.Normalize();
+
+//            JVector groundFwd = JVector.Cross(groundLeft, groundUp);
+
+//            JVector wheelPointVel = BodyCar.LinearVelocity +
+//                    JVector.Cross(BodyCar.AngularVelocity, JVector.Transform(Position, BodyCar.Orientation));
+
+//            // rimVel=(wxr)*v
+//            JVector rimVel = angVel * JVector.Cross(wheelLeft, groundPos - worldPos);
+//            wheelPointVel += rimVel;
+
+//            JVector worldVel = worldBody.LinearVelocity +
+//             JVector.Cross(worldBody.AngularVelocity, groundPos - worldBody.Position);
+
+//            wheelPointVel -= worldVel;
+
+//            // sideways forces
+//            float noslipVel = 0.1f;
+//            float slipVel = 0.1f;
+//            float slipFactor = 0.7f;
+
+//            float smallVel = 3;
+//            float friction = SideFriction;
+
+//            float sideVel = JVector.Dot(wheelPointVel, groundLeft);
+
+//            if ((sideVel > slipVel) || (sideVel < -slipVel))
+//                friction *= slipFactor;
+//            else
+//                if ((sideVel > noslipVel) || (sideVel < -noslipVel))
+//                    friction *= 1.0f - (1.0f - slipFactor) * (System.Math.Abs(sideVel) - noslipVel) / (slipVel - noslipVel);
+
+//            if (sideVel < 0.0f)
+//                friction *= -1.0f;
+
+//            if (System.Math.Abs(sideVel) < smallVel)
+//                friction *= System.Math.Abs(sideVel) / smallVel;
+
+//            float sideForce = -friction * totalForceMag;
+
+//            extraForce = sideForce * groundLeft;
+//            force += extraForce;
+
+//            // fwd/back forces
+//            friction = ForwardFriction;
+//            float fwdVel = JVector.Dot(wheelPointVel, groundFwd);
+
+//            if ((fwdVel > slipVel) || (fwdVel < -slipVel))
+//                friction *= slipFactor;
+//            else
+//                if ((fwdVel > noslipVel) || (fwdVel < -noslipVel))
+//                    friction *= 1.0f - (1.0f - slipFactor) * (System.Math.Abs(fwdVel) - noslipVel) / (slipVel - noslipVel);
+
+//            if (fwdVel < 0.0f)
+//                friction *= -1.0f;
+
+//            if (System.Math.Abs(fwdVel) < smallVel)
+//                friction *= System.Math.Abs(fwdVel) / smallVel;
+
+//            float fwdForce = -friction * totalForceMag;
+
+//            extraForce = fwdForce * groundFwd;
+//            force += extraForce;
+
+//            // fwd force also spins the wheel
+//            JVector wheelCentreVel = BodyCar.LinearVelocity +
+//             JVector.Cross(BodyCar.AngularVelocity, JVector.Transform(Position, BodyCar.Orientation));
+
+//            angVelForGrip = JVector.Dot(wheelCentreVel, groundFwd) / Radius;
+//            torque += -fwdForce * Radius;
+
+//            // add force to car
+//            BodyCar.AddForce(force, groundPos + 0.5f * JVector.Up);
+
+//            // add force to the world
+//            if (!worldBody.IsStatic)
+//            {
+//                worldBody.AddForce(force*(-1) * 0.01f, groundPos);
+//            }
+
+//        }
+
+//        private bool RaycastCallback(RigidBody body, JVector normal, float frac)
+//        {
+//            return (body != BodyCar);
+//        }
+
+//    }
+//}
---
+++ BlackKitten/PersianCommon/PersianCore/Graphics/Meshes/Weapons/Weapon.cs
@@ -0,0 +1,283 @@
+﻿//using System;
+//using System.ComponentModel;
+//using PersianCore.Meshes;
+//using PersianCore.Timer;
+//using Microsoft.Xna.Framework;
+//using Microsoft.Xna.Framework.Graphics;
+//using PersianCore.Enviroment;
+//using PersianCore.Weapons;
+//using PersianCore.Graphics.Particles;
+
+//namespace PersianCore.Weapons
+//{
+//    public enum WeaponType { NOP, Pistol, Auto }
+//    public enum WeaponState { Idle, Fire , Reload }
+
+//    public class Weapon : Node
+//    {
+//        public Vector3 FixPosition = new Vector3(2.951267f, -0.5326776f, -0.2061971f);
+
+//        #region Fields & Properties
+
+//        SmartTimer smartTimer;
+//        BulletsManager bulletManager;
+//        Billboard Muzzle;
+//        //pointer to particleSystem
+//        ParticleSystem ParticleSystem
+//        {
+//            get;
+//            set;
+//        }
+
+//        int bindedBoneIndex;
+//        public int BindedBoneIndex
+//        {
+//            get
+//            {
+//                return this.bindedBoneIndex;
+//            }
+//            set
+//            {
+//                this.bindedBoneIndex = value;
+//            }
+//        }
+
+//        WeaponState _weaponState;
+//        public WeaponState weaponState
+//        {
+//            get
+//            {
+//                return this._weaponState;
+//            }
+//            set
+//            {
+//                this._weaponState = value;
+//            }
+//        }
+        
+//        WeaponType _weaponType;
+//        public WeaponType weaponType
+//        {
+//            get
+//            {
+//                return this._weaponType;
+//            }
+//            set
+//            {
+//                this._weaponType = value;
+//            }
+//        }
+               
+//        Vector3 muzzlePosition;
+//        public Vector3 MuzzlePosition
+//        {
+//            get
+//            {
+//                return this.muzzlePosition;
+//            }
+//            set
+//            {
+//                this.muzzlePosition = value;
+//            }
+//        }
+
+//        Vector3 bulletVelocity;
+//        public Vector3 BulletVelocity
+//        {
+//            get
+//            {
+//                return this.bulletVelocity;
+//            }
+//            set
+//            {
+//                this.bulletVelocity = value;
+//            }
+//        }
+
+//        Vector3 bulletShellVelocity;
+//        public Vector3 BulletShellVelocity
+//        {
+//            get
+//            {
+//                return this.bulletShellVelocity;
+//            }
+//            set
+//            {
+//                this.bulletShellVelocity = value;
+//            }
+//        }
+
+//        #endregion
+
+//        #region Constructor/Destructor
+        
+//        public Weapon()
+//            : base()
+//        {
+//            InitializeFields();
+//        }
+
+//        private void InitializeFields()
+//        {
+//            this.smartTimer = new SmartTimer(TimeSpan.FromSeconds(3.0d));
+//            this.bindedBoneIndex = 23;//Assign to Right Hand
+//            this._weaponState = WeaponState.Idle;
+//            this._weaponType = WeaponType.NOP;
+//            this.bulletVelocity = new Vector3(1000, 1000, 1000);
+//            this.bulletShellVelocity = new Vector3(5, 5, 5);
+//            this.muzzlePosition = new Vector3(8.4f, 0.0f, 0.0f);
+//            this.Muzzle = new Billboard();
+//        }
+        
+//        #endregion
+
+//        #region Load
+
+//        public void Load(ref string HResult)
+//        {
+//            Model bulletModel = CoreShared.Content.Load<Model>(@"Models\Utilities\Bullet\Bullet");
+//            Model bulletShellModel = CoreShared.Content.Load<Model>(@"Models\Utilities\Bullet\BulletShell");
+//            //Velocity has been set in initialize so be sure bulletManager load here
+//            this.bulletManager = new BulletsManager(this.bulletVelocity, this.bulletShellVelocity, TimeSpan.FromSeconds(1.0d), TimeSpan.FromSeconds(4.0d));
+//            this.bulletManager.Load(bulletModel, bulletShellModel, ref HResult);
+//            if (HResult != null)
+//            {
+//                return;
+//            }
+
+//            this.Muzzle.Load(CoreShared.MuzzleTextures);
+//        }
+        
+//        #endregion
+
+//        #region Update
+
+//        public void Update(GameTime gameTime, Mesh BindTo, ref Matrix World)
+//        {
+//            bool UseBinding = false;
+//            if (BindTo != null)
+//            {
+//                UseBinding = true;
+//            }
+//            this.smartTimer.Update(gameTime);
+
+//            #region Get Transform of Bullets and Muzzles from BoneIndex
+
+//            if (UseBinding)
+//            {
+//                if (BindTo.BonesData != null)
+//                {
+//                    //This Binded object is skinned so get Transform of Bone index
+//                    Matrix BoneMatrix;
+//                    try
+//                    {
+//                        BoneMatrix = BindTo.BonesData[bindedBoneIndex].World;
+//                    }
+//                    catch
+//                    {
+//                        Logger.WriteError(string.Format("BindedBoneIndex of weapon named {0} is out of range", BindTo.ID));
+//                        return;
+//                    }
+//                    World *=  BoneMatrix * BindTo.World;
+//                }
+//                else
+//                {
+//                    //This Binded object is not skinned so get Transform
+//                    World *= BindTo.World;
+//                }
+//            }
+
+//            #endregion
+
+//            #region Set State
+
+//            if (this._weaponState == WeaponState.Reload)
+//            {
+//                if (smartTimer.State == SmartTimer.TimerState.Disposed)
+//                {
+//                    this.bulletManager.Reload();
+//                    this._weaponState = WeaponState.Idle;
+//                }
+//            }
+//            else if (UseBinding && BindTo.UpperBehavioral == PersianCore.Meshes.Mesh.Behvioral.Shooting)
+//            {
+//                this._weaponState = WeaponState.Fire;
+//            }
+//            else
+//            {
+//                if (BaseShared.SlavedByEngine)
+//                {
+//                    //Just set Idle fo engine not in editor...this is for better checking in visual editor
+//                    this._weaponState = WeaponState.Idle;
+//                }
+//            }
+
+//            #endregion
+
+//            if (this._weaponState == WeaponState.Fire)
+//            {
+//                #region Fire Bullet
+
+//                var bulletStartPosition = Vector3.Transform(this.muzzlePosition, World);
+//                var bulletShellPosition = Vector3.Transform(this.muzzlePosition + new Vector3(1.2f, 0.0f, 7.5f), World);
+//                this.Muzzle.Position = bulletStartPosition;
+//                this.Muzzle.Update();
+//                if (BaseShared.SlavedByEngine)
+//                {
+//                    if (this.bulletManager.FireBullet(gameTime, bulletStartPosition, bulletShellPosition,
+//                        new Vector3(BindTo.BonesData[1].Rotation.X, BindTo.Rotation.Y, BindTo.BonesData[1].Rotation.Z)))
+//                    {
+//                        this.smartTimer.Start();
+//                        this._weaponState = WeaponState.Reload;
+//                        if (BindTo != null)
+//                        {
+//                            //Allow Binded to Update his behavioral
+//                            BindTo.UpperBehavioral = Meshes.Mesh.Behvioral.Reloading;
+//                        }
+//                    }
+//                }
+
+//                #endregion
+//            }
+
+//            this.bulletManager.Update(gameTime);
+//        }
+
+//        #endregion
+
+//        #region Draw
+
+//        public void DrawToGBuffer(GraphicsDevice GDevice)
+//        {
+//            bulletManager.DrawToGBuffer(GDevice);
+//        }
+
+//        public void ReConstructShading(GraphicsDevice GDevice, RenderTarget2D LightMap)
+//        {
+//            bulletManager.ReConstructShading(GDevice, LightMap);
+//        }
+
+//        public void DrawBlended(GraphicsDevice GDevice)
+//        {
+//            if (this._weaponState == WeaponState.Fire)
+//            {
+//                this.Muzzle.Draw(GDevice);
+//            }
+//        }
+          
+//        #endregion
+
+//        #region Dispose
+
+//        protected override void Dispose(bool disposing)
+//        {
+//            if (disposing)
+//            {
+//                this.Muzzle.Dispose();
+//            }
+//            base.Dispose(disposing);
+//        }
+
+//        #endregion
+//    }
+//}
