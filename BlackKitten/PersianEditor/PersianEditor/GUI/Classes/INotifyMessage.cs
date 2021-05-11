51b7cda98b30c798d0d1a5ea543fe168402ed16a
PooyaEimandar <pooya.eimandar@live.com>
2021-05-11 13:00:42 +0430

    PersianEngine (codename: BlackKitten)

---
+++ BlackKitten/PersianEditor/PersianEditor/GUI/Classes/INotifyMessage.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel;
+
+namespace PersianEditor.Classes
+{
+    public class INotifyMessage : INotifyPropertyChanged
+    {
+        #region Fields
+
+        string message;
+        public string Message
+        {
+            get
+            {
+                return this.message;
+            }
+            set
+            {
+                this.message = value;
+                NotifyPropertyChanged("Message");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Constructor
+
+        public INotifyMessage()
+            : this(string.Empty)
+        {
+        }
+
+        public INotifyMessage(string Message)
+        {
+            this.message = Message;
+            this.PropertyChanged += new PropertyChangedEventHandler(OnPropertyChanged);
+        }
+        
+        #endregion
+
+        #region Events
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+        #region Overrides
+
+        public override string ToString()
+        {
+            return this.message.ToString();
+        }
+
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            
+        }
+
+        #endregion
+    }
+}
---
+++ BlackKitten/PersianEditor/PersianEditor/GUI/GUI/_Base Inherit/MenuUC.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Controls;
+
+namespace PersianEditor.UserControls
+{
+    public partial class MenuUC : UserControl
+    {
+        #region Fields & Properties
+
+        public event EventHandler<CmdEventArgs> OnMenuClicked;
+        public bool IsInDesignMode
+        {
+            get
+            {
+                return DesignerProperties.GetIsInDesignMode(this);
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public MenuUC()
+        {
+            InitializeComponent();
+            if (!this.IsInDesignMode)
+            {
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        private void OnMenuItemClick(object sender, Telerik.Windows.RadRoutedEventArgs e)
+        {
+            string Name = e.OriginalSource.GetType().GetProperty("Name").GetValue(e.OriginalSource, null).ToString();
+            OnMenuClicked(this, new CmdEventArgs(Name));
+        }
+
+        #endregion
+    }
+}
