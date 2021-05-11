bc3550f4f06142a62b8ce5213f4aa31ebf488e83
PooyaEimandar <pooya.eimandar@live.com>
2021-05-11 13:00:24 +0430

    PersianEngine (codename: BlackKitten)

---
+++ BlackKitten/PersianEditor/PersianEditor/GUI/GUI/Bar/NotifyListUC.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Controls;
+using PersianEditor.Classes;
+
+namespace PersianEditor.UserControls
+{
+    public partial class NotifyListUC : UserControl
+    {
+        #region Fields & Properties
+
+        public bool IsInDesignMode
+        {
+            get
+            {
+                return DesignerProperties.GetIsInDesignMode(this);
+            }
+        }
+
+        ObservableCollection<INotifyMessage> NotiftyList;
+
+        #endregion
+
+        #region Constructor/Destructor
+
+        public NotifyListUC()
+        {
+            InitializeComponent();
+            if (!IsInDesignMode)
+            {
+                this.NotiftyList = new ObservableCollection<INotifyMessage>();
+                this.listBox.ItemsSource = this.NotiftyList;
+            }
+        }
+
+        ~NotifyListUC()
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Clear()
+        {
+            this.NotiftyList.Clear();
+        }
+
+        public void Add(string Message)
+        {
+            this.NotiftyList.Add(new INotifyMessage(Message));
+        }
+
+        public void ClearThenAdd(string Message)
+        {
+            this.NotiftyList.Clear();
+            this.NotiftyList.Add(new INotifyMessage(Message));
+        }
+
+        #endregion
+    }
+}
